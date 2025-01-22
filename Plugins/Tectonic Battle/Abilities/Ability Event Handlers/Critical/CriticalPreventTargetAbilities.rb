cdc7c9ff4333191b8cdec7d71d80563b9a5763d6
ZinniaGameDev <xeuorux@gmail.com>
2025-01-21 20:25:50 -0700

    Refactor Crit Immunity abilities to work off flag

--- Plugins/Tectonic Battle/Abilities/Ability Event Handlers/Critical/CriticalPreventTargetAbilities.rb
+++ Plugins/Tectonic Battle/Abilities/Ability Event Handlers/Critical/CriticalPreventTargetAbilities.rb
@@ -1,7 +1,9 @@
-BattleHandlers::CriticalPreventTargetAbility.add(:BATTLEARMOR,
-  proc { |ability, _user, _target, _battle|
-      next true
-  }
-)
-
-BattleHandlers::CriticalPreventTargetAbility.copy(:BATTLEARMOR, :SHELLARMOR, :IMPERVIOUS, :STEELYSHELL, :LIVINGARMOR, :UNCONQUERABLE, :LEVIATHAN)
+BattleHandlers::LoadDataDependentAbilityHandlers += proc {
+  GameData::Ability.getByFlag("CritImmunity").each do |abilityID|
+      BattleHandlers::CriticalPreventTargetAbility.add(abilityID,
+        proc { |ability, _user, _target, _battle|
+            next true
+        }
+      )
+  end
+}
