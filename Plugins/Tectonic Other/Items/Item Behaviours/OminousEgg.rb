aeca9a38b885082b4974a388eed3d65068f0a97d
ZinniaGameDev <xeuorux@gmail.com>
2025-02-10 23:04:13 -0700

    OMINOUSEGG rewards tweaks
    Increased threshold for the more powerful rewards
    Added a PRISMSTONE at the 2nd threshold

--- Plugins/Tectonic Other/Items/Item Behaviours/OminousEgg.rb
+++ Plugins/Tectonic Other/Items/Item Behaviours/OminousEgg.rb
@@ -18,22 +18,27 @@ def checkForOminousEggRewards
     value = getGlobalVariable(TRAINERS_PERFECTED_GLOBAL_VAR)
     rewards = []
 
-    if value >= 25 && $PokemonGlobal.ominous_egg_stage == 0
+    if value >= 30 && $PokemonGlobal.ominous_egg_stage == 0
         rewards.push(:LIFEORB)
         $PokemonGlobal.ominous_egg_stage += 1
     end
 
-    if value >= 50 && $PokemonGlobal.ominous_egg_stage == 1
+    if value >= 60 && $PokemonGlobal.ominous_egg_stage == 1
+        rewards.push(:PRISMSTONE)
+        $PokemonGlobal.ominous_egg_stage += 1
+    end
+
+    if value >= 90 && $PokemonGlobal.ominous_egg_stage == 2
         rewards.push(:MASTERBALL)
         $PokemonGlobal.ominous_egg_stage += 1
     end
 
-    if value >= 75 && $PokemonGlobal.ominous_egg_stage == 2
+    if value >= 120 && $PokemonGlobal.ominous_egg_stage == 3
         rewards.push(:RELICSTATUE)
         $PokemonGlobal.ominous_egg_stage += 1
     end
 
-    if value >= 100 && $PokemonGlobal.ominous_egg_stage == 3
+    if value >= 150 && $PokemonGlobal.ominous_egg_stage == 4
         rewards.push(:SHINYCHARM)
         $PokemonGlobal.ominous_egg_stage += 1
     end
@@ -55,10 +60,12 @@ def playOminousEggCutscene
     when 1
         pbMessage(_INTL("\\i[OMINOUSEGG]...sacrifice..."))
     when 2
-        pbMessage(_INTL("\\i[OMINOUSEGG]...urge to dominate..."))
+        pbMessage(_INTL("\\i[OMINOUSEGG]...strength in avarice..."))
     when 3
-        pbMessage(_INTL("\\i[OMINOUSEGG]...to stand above others..."))
+        pbMessage(_INTL("\\i[OMINOUSEGG]...urge to dominate..."))
     when 4
+        pbMessage(_INTL("\\i[OMINOUSEGG]...to stand above others..."))
+    when 5
         pbMessage(_INTL("\\i[OMINOUSEGG]...nothing can hide from you..."))
     end
 end
